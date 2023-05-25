5a3a687ab36ce42e61dd0a8bee1bffa7d1220a8c
Danladi Etu <danladietu36@gmail.com>
2023-05-25 05:37:01 -0700

    added

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+A script that uses 'https://jsonplaceholder.typicode.com/todos/1'
+RESTFUL API for a given employee ID, and returns information 
+about his/her TODO list progress
+"""
+import requests
+import re
+import sys
+
+
+API = "https://jsonplaceholder.typicode.com/todos/1"
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            id = int(sys.argv[1])
+            user_res = requests.get('{}/users/{}'.format(API, id)).json()
+            todos_res  = requests.get('{}/todods'.format(API)).json()
+            username = user_res.get('name')
+            todos = list(filter(lamda x: x.get('completed'), todos))
+            print(
+                    'Employee {} is done with tasks({}/{}:'.format(
+                        username,
+                        len(todos_done),
+                        len(todos)
+                        )
+                    )
+            for todo_done in todos_done:
+                print('\t{}'.format(todo_done.get('title')))
