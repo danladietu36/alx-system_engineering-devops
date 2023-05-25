b5c3a194050ec9ce73adee230de8dc8ce4cf42cc
Danladi Etu <danladietugithub@gmal.com>
2023-05-25 12:14:54 -0700

    added

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Using a REST API for todo lists of employees"""
+"""Accessing a REST API for todo lists of employees"""
 
 import requests
 import sys
@@ -7,17 +8,17 @@ import sys
 
 if __name__ == '__main__':
     employeeId = sys.argv[1]
-    base_url = "https://jsonplaceholder.typicode.com/users"
-    url = base_url + "/" + employeeId
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + employeeId
 
-    response = requests.get('url')
+    response = requests.get(url)
     username = response.json().get('username')
 
-    todo_url = url + "/todos"
-    response = requests.get('todo_url')
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
     tasks = response.json()
 
-    with open('{}.csv'.format(employeeId), 'w') as fileName:
+    with open('{}.csv'.format(employeeId), 'w') as file:
         for task in tasks:
             file.write('"{}","{}","{}","{}"\n'
                        .format(employeeId, username, task.get('completed'),
