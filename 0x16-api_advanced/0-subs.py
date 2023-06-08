028cd02aaf33563ef6e9036e3d7fffd64f75093b
Danladi Etu <danladietugithub@gmal.com>
2023-06-07 17:41:24 -0700

    added

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
 """
-python script to return number of subscribers for a given
-subreddit
+A python script to return number of subscribers for a given subreddit
 """
-
 
 from requests import get
 
 
-def num_of_subscribers(subreddit):
+def number_of_subscribers(subreddit):
     """
-    Function that queries Reddit API and return number of
-    total subscribers for a given reddit.
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
     """
 
-     if subreddit is None or not isinstance(subreddit, str):
-         return 0
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
 
     user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
