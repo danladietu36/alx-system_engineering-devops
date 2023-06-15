3d970b5d1ead5f479a57f7671e14df6526de663f
Danladi Etu <danladietugithub@gmail.com>
2023-06-15 09:07:41 -0700

    added

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,7 @@
 # A puppet script to increase Nginx traffic
 
-exec { 'fix--nginx':
+#This code increases ULIMIT of default file
+exec { 'fix--for-nginx':
     command => 'sed -i "s/15/4096/" /etc/default/nginx',
     path    => '/usr/local/bin/:bin/'
 }
