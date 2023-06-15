fedc6c9f70bd1c75afc48a9cc13a4d3ea56fb226
Danladi Etu <danladietugithub@gmail.com>
2023-06-15 09:15:00 -0700

    added

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,13 @@
-# A puppet script taht enable user holberton to login and open files
+# This puppet script allows user holberton to login and open files without error
 
 # This code increases hard file limit for user holberton
 exec { 'increase-hard-file-limit-for-holberton-user':
-    command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
-    path    => '/usr/local/bin/:/bin/'
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# This code increases soft file limit for holberton
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
